aec9955e369e798f55d4a6482c0a5d2d5642060f
mariano abizanda <mariano.abizanda@gmail.com>
2022-07-14 00:33:31 -0300

    primer desafio complementario

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+// Desafio complementario
+
+
+let opcion 
+while(opcion != 2){
+    opcion = prompt("\nopcion 1: sacar turno \nopcion 2: salir")
+    for(let i = 1; i < 5; i++){
+        if(opcion == 1)
+        nombre = prompt("Ingrese su nombre para sacar un turno")
+        resultado = alert('\nPaciente:' + nombre + "\nTu turno es el Nº: " +i )
+
+       
+        
+    }
+alert("no quedan turnos disponibles por hoy")
+}
+
+
