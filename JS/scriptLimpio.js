a190210c3d3a2820f332f324fdfd9489e8e30010
mariano abizanda <mariano.abizanda@gmail.com>
2022-08-22 15:56:46 -0300

    x

--- JS/scriptLimpio.js
+++ JS/scriptLimpio.js
@@ -1,31 +1,31 @@
-let opciones = 0
-let productos = []
-let producto = {}
-let unidades = 0
-let total = 0
-let categoriaIngresada = ""
-let nombreIngresado = ""
+// let opciones = 0
+// let productos = []
+// let producto = {}
+// let unidades = 0
+// let total = 0
+// let categoriaIngresada = ""
+// let nombreIngresado = ""
 
-class Bebida {      
-  constructor(producto, precio, stock, categoria) {
-      this.nombre = producto,
-      this.precio = precio,
-      this.stock = stock
-      this.categoria = categoria //agregar la propiedad foto: asi también la podes renderizar dinamicamente
-  }
-}
+// class Bebida {      
+//   constructor(producto, precio, stock, categoria) {
+//       this.nombre = producto,
+//       this.precio = precio,
+//       this.stock = stock
+//       this.categoria = categoria //agregar la propiedad foto: asi también la podes renderizar dinamicamente
+//   }
+// }
 
-const cerveza1 = new Bebida('Quilmes', 100, 500, 'Cerveza')
-const cerveza2 = new Bebida('Brahma', 120, 450, 'Cerveza')
-const cerveza3 = new Bebida('Imperial', 150, 370, 'Cerveza')
-const vino1 = new Bebida('Alamos', 280, 220, 'Vino')
-const vino2 = new Bebida('Aime', 200, 350, 'Vino')
-const vino3 = new Bebida('Benjamin', 180, 340, 'Vino')
-const aperitivo1 = new Bebida('Fernet Branca', 900, 300, 'Aperitivo')
-const aperitivo2 = new Bebida('Campari', 820, 240, 'Aperitivo')
+// const cerveza1 = new Bebida('Quilmes', 100, 500, 'Cerveza')
+// const cerveza2 = new Bebida('Brahma', 120, 450, 'Cerveza')
+// const cerveza3 = new Bebida('Imperial', 150, 370, 'Cerveza')
+// const vino1 = new Bebida('Alamos', 280, 220, 'Vino')
+// const vino2 = new Bebida('Aime', 200, 350, 'Vino')
+// const vino3 = new Bebida('Benjamin', 180, 340, 'Vino')
+// const aperitivo1 = new Bebida('Fernet Branca', 900, 300, 'Aperitivo')
+// const aperitivo2 = new Bebida('Campari', 820, 240, 'Aperitivo')
 
-let listaProductos = ['Cerveza','Vino','Aperitivo']
-let listaBebidas = [cerveza1, cerveza2, cerveza3, vino1, vino2, vino3, aperitivo1, aperitivo2]
+// let listaProductos = ['Cerveza','Vino','Aperitivo']
+// let listaBebidas = [cerveza1, cerveza2, cerveza3, vino1, vino2, vino3, aperitivo1, aperitivo2]
 
 
 
